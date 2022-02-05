671d23b847a56276684745df072e8ad37a69d7f7
Elvin Taghizade Taghi <elvintaghiyev184@gmail.com>
2022-02-05 11:57:11 +0400

    initial commit

---
+++ src/main/java/az/et/eurekaserver/config/AppConfig.java
@@ -0,0 +1,21 @@
+package az.et.eurekaserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/eureka/**")
+                .addResourceLocations(
+                        "/eureka/",
+                        "classpath:/static/eureka/"
+                );
+    }
+}
